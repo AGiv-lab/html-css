fa9c82052041379f6bae4aa537f4779ecd425f48
Amity Givens <webdev111222@gmail.com>
2026-01-12 23:13:27 -0800

    Add JS welcome message with user prompt and conditional response

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("JavaScript connected successfully 🚀");
+console.log("JS is working");
+
+const welcomeMessage = document.querySelector("#welcome-message");
+
+const userReason = prompt(
+  "Woof! 🐾 (wags tail)\nTell us why you're here:\n\n" +
+  "1. I am looking to adopt\n" +
+  "2. I am looking to volunteer\n" +
+  "3. I am looking to surrender a pet"
+);
+
+if (userReason === "1") {
+  welcomeMessage.textContent =
+    "Nice to meet you future adopter — paw shake on it! 🐾";
+
+ } else if (userReason === "2") {
+  welcomeMessage.textContent =
+    "Nice to meet you future volunteer — paw shake on it! 🐾";
+
+} else if (userReason === "3") {
+  welcomeMessage.textContent =
+    "Thanks for trusting us. Let's help your pet find a loving home 🐶";
+
+} else {
+  welcomeMessage.textContent =
+    "Woof! 🐾 Take a look around and let us know how we can help.";
+}
+    
